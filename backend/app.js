20e8326d5de7d9eb6fb0af3d63c5e6371a751fd2
Koded-P <edosepray@gmail.com>
2021-11-11 00:24:05 +0000

    Login/signup

--- backend/app.js
+++ backend/app.js
@@ -10,15 +10,23 @@ const app = express();
 
 mongoose.connect('mongodb+srv://piiquante:S1gvYrcup8TUxU8F@cluster0.5pmhv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
   .then(() => {
-    console.log('Successfully connected to MongoDB Atlas!');
+    console.log('Successfully connected to MongoDB Atlas! hurray');
   })
   .catch((error) => {
     console.log('Unable to connect to MongoDB Atlas!');
     console.error(error);
   });
 
-app.use((req, res) => {
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+
+app.use((req, res, next) => {
     res.json({ message: 'Your request was successful'});
+    next();
 });
 
 app.use('/api/auth', userRoutes);

054aa097443b5d36af3e5ecf368537b3bf2c980a
Koded-P <edosepray@gmail.com>
2021-11-11 01:55:37 +0000

    added images folder

--- backend/routes/sauce.js
+++ backend/routes/sauce.js
@@ -8,8 +8,8 @@ const sauceCtrl = require ('../controllers/sauce');
 
 router.get("/", auth, sauceCtrl.getAllSauces);
 router.get('/:id', auth, sauceCtrl.getOneSauce);
-router.post("/", auth, multer, checkSauceInput, sauceCtrl.createSauce);
-router.put("/:id", auth, multer, checkSauceInput, sauceCtrl.updateSauce)
+router.post("/", auth, multer, sauceCtrl.createSauce);
+router.put("/:id", auth, multer, sauceCtrl.updateSauce)
 router.delete("/:id", auth, sauceCtrl.deleteSauce)
 router.post("/:id/like", auth, sauceCtrl.likeDislikeSauce)
 
